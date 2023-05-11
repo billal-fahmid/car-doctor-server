b6a54ad2a820bd5109646f9df973753df8f0d899
billal-fahmid <billalcooom@gmail.com>
2023-05-11 18:04:33 +0600

    update and add mongoDB database

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const app= express()
@@ -27,12 +27,81 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const servicesCollection = client.db('carsDoctor').collection('services');
+    const checkOutCollection = client.db('carsDortor').collection('checkOut');
+
+
+    app.get('/services' , async(req , res) =>{
+      const cursor = servicesCollection.find()
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/services/:id' , async (req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+    const options = {
+      // Include only the `title` and `imdb` fields in the returned document
+      projection: {  title: 1, price:1 , service_id:1 ,img:1},
+    };
+      const result = await servicesCollection.findOne(query,options);
+      res.send(result)
+    })
+
+
+
+    app.get('/checkouts' ,async (req, res) =>{
+      // console.log(req.query.email)
+      let query = {};
+      if(req.query?.email){
+        query= {email: req.query.email}
+      }
+      const result = await checkOutCollection.find(query).toArray()
+      res.send(result)
+    })
+
+
+
+    app.post('/checkouts' , async (req ,res) =>{
+      const info = req.body;
+      console.log(info)
+      const result = await checkOutCollection.insertOne(info);
+      res.send(result)
+    })
+
+    app.patch('/checkouts/:id' , async(req,res)=>{
+      const id = req.params.id
+      const filter = {_id: new ObjectId(id)}
+      // const options = {upsert:true}
+      const updatedInfo =req.body;
+      console.log(updatedInfo)
+      const updateDoc={
+        $set:{
+          status:updatedInfo.status
+        }
+      }
+      const result =await checkOutCollection.updateOne(filter,updateDoc)
+      res.send(result)
+      
+    })
+
+    app.delete('/checkouts/:id' , async (req, res) =>{
+      const id = req.params.id 
+      const query = {_id: new ObjectId(id)}
+      const result = await checkOutCollection.deleteOne(query);
+      res.send(result)
+    })
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
